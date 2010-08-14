9774b1cd9256a61b6bdc522c171cd5aeb194d4df
JL Falcone <jean-luc.falcone@unige.ch>
2010-08-14 15:45:38 +0200

    Project layout

---
+++ project/build/Project.scala
@@ -0,0 +1,17 @@
+import sbt._
+
+class ScalaGraphProject(info: ProjectInfo) extends DefaultProject(info)
+{
+
+  val scalatest = "org.scalatest" % "scalatest" % "1.2"
+
+  //override val mainClass = Some("aggreg.Exp")
+
+  override def mainScalaSourcePath = "src"
+  override def mainResourcesPath = "resources"
+        
+  override def testScalaSourcePath = "test-src"
+  override def testResourcesPath = "test-resources"
+
+
+}
